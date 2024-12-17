52c8dc7a9ae1dfe49bd83e7150d92c0fee0650bc
Catherine <catherine.dieval@hotmail.fr>
2024-12-17 23:19:35 +0100

    Added data fetching with useSWR in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (!data || isLoading) {
+    return "Loading...";
+  }
+
+  const { firstName, lastName, age, profession } = data;
+  
+  return (
+    <>
+      <h1>Hello from Next.js API routes!</h1>
+      <h2>Click on the reload button in the browser to see a a random person displayed.</h2>
+      <br/>
+      <p>{firstName}, {lastName}, {age}, {profession}</p>
+    </>
+);
 }
